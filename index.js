e7a8027d16daa254092c9ec405a7e0cba986f4da
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-10 21:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var recipes = new Object ();
 
 function updateObjectWithKeyAndValue(object, key, value)  {
   var object = [key, value]
+  var objectTwo = object + [keyTwo, valueTwo]
   return object
 }
