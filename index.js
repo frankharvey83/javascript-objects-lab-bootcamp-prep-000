5dd2e39680cd6bd4d578db50cfb0fb91b033e4e6
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-10 22:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var recipes = new Object ();
 function updateObjectWithKeyAndValue(object, key, value)  {
   var object = [key, value]
   return object
-}
+};
+ 
+ 
+Object.assign({}, object, { key: ['value', 'newvalue'] })
 
+object
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var object = [key, value]
   return obj
 }
 
-const newobj = { newkey: newvalue }
 
-destructivelyUpdateObjectWithKeyAndValue(newobj, 'flour', '3 cups')
