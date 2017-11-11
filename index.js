456415e6493f9873011bedc0096f639ddb9392a3
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-11 02:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key, value) {
   object = { key: value };
   delete object.key;
   return object;
