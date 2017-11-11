57115d4137c1c808ec362e2c18a21d6205b9d831
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-11 00:38:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value)  {
-  var object = {
-    key: value
-  };
+  var object = { key: value };
   return obj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  var object = {
-    key: value
-  }
+  var object = { key: value };
   object.newkey = value;
-  return object;
 }
 
 
 
 function deleteFromObjectByKey(object, key){
-
+  var object = {}
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-
+function destructivelyDeleteFromObjectByKey(object, key, value){
+  var object = { key: value };
+  delete.object.key;
+  return object;
 }
