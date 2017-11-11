b7edad80c58274286483ba409764c4498df02128
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-11 00:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function updateObjectWithKeyAndValue(obj, key, value)  {
   };
   return obj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
